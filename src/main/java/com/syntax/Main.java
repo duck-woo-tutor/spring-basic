c6aca0e5038ed6e7ee0cf7c898cf799654357118
accidentally <woozoolsd@gmail.com>
2022-11-07 22:33:50 +0900

    조건문 - if-else, switch, ternary operator

--- src/main/java/com/syntax/Main.java
+++ src/main/java/com/syntax/Main.java
@@ -4,53 +4,34 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        // 산술 연산자
-        int num1 = 10;
-        int num2 = 5;
+        // if - else
+        int num1 = 50;
+        if (num1 > 0) {
+            System.out.println("100보다 큰 수입니다");
+        } else {
+            System.out.println("100보다 작은 수입니다.");
+        }
 
-        System.out.println(num1 + num2); // 더하기 연산
-        System.out.println(num1 - num2); // 빼기 연산
-        System.out.println(num1 * num2); // 곱하기 연산
-        System.out.println(num1 / num2); // 나누기 연산
-        System.out.println(num1 % num2); // 나머지 연산
+        // switch
+        switch (num1) {
+            case 0:
+                System.out.println("응애 나 0");
+                break;
+            case 1:
+                System.out.println("응애 나 1");
+                break;
+            case 2:
+                System.out.println("응애 나 2");
+                break;
+            default:
+                System.out.println("응애 나 몰라");
+                break;
+        }
 
-        // 대입 연산자
-        num1 += num2; // num1 = num1 + num2
-        System.out.println(num1);
+        // 삼항 연산자
+        int a = 5;
 
-        num1 -= num2; // num1 = num1 - num2
-        System.out.println(num1);
-
-        num1 *= num2; // num1 = num1 * num2
-        System.out.println(num1);
-
-        num1 /= num2; // num1 = num1 / num2
-        System.out.println(num1);
-
-        num1 %= num2; // num1 = num1 % num2
-        System.out.println(num1);
-
-        // 관계 연산자
-        num1 = 10;
-        num2 = 20;
-        int num3 = 10;
-
-        System.out.println(num1 > num2); // 10 > 20
-        System.out.println(num1 >= num3); // 10 >= 10
-        System.out.println(num1 < num2); // 10 < 20
-        System.out.println(num1 <= num2); // 10 <= 20
-        System.out.println(num1 == num3); // 10 == 10
-        System.out.println(num1 != num2); // 10 != 20
-
-        // 논리 연산자
-        boolean a = true;
-        boolean b = false;
-
-        System.out.println(a && b);
-// &&는 두가지 모두 참일 경우에는 true를, 그렇지 않을 경우에는 false를 반환합니다.
-        System.out.println(a || b);
-// ||는 두가지 모두 거짓일 경우에는 false를, 그렇지 않을 경우에는 true를 반환합니다.
-        System.out.println(!b);
-// !는 피연산자의 논리값을 바꿉니다. true는 false로 , false는 true로 반환합니다.
+        String reuslt = (a < 10) ? "10보다 작습니다." : "10보다 큽니다.";
+        System.out.println(reuslt);
     }
 }
